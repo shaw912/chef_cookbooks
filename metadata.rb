d6dfe33b62a290f01faad9af1f64fd24f0cab8a2
shaw <shashank_g2006@yahoo.com>
2017-07-18 14:32:55 +0000

    Version 0.2.0 release -added emacs and updated metadata file

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name 'workstation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Shaw'
+maintainer_email 'shashank_g2006@yahoo.com'
 license 'all_rights'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '0.1.0'
-
+version '0.2.0'
+# Version is in symantec fomat of 'major.minor.patch'
+# Major Versions are not backward compatible, but minor and patches are backward compatible
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/workstation/issues' if respond_to?(:issues_url)
